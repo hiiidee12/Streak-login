9d25098730e7a1653b7c9e85d068c0ae5203dd73
hiiidee12 <125862664+hiiidee12@users.noreply.github.com>
2026-01-04 22:04:17 +0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,7 @@
+export const CONFIG = {
+  BASE_CHAIN_ID_DEC: 8453,
+  BASE_CHAIN_ID_HEX: "0x2105",
+  BASE_RPC: "https://mainnet.base.org",
+  LS_KEY: "daily_checkin_last_date",
+  EXPLORER_TX: (hash) => `https://basescan.org/tx/${hash}`,
+};
