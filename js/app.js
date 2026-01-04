c462237f3f400599bad11b4e83fede48d89632b9
hiiidee12 <125862664+hiiidee12@users.noreply.github.com>
2026-01-04 22:09:24 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,14 +1,9 @@
+// js/app.js
 import { initFarcaster } from "./farcaster.js";
 import { initWalletUI } from "./wallet.js";
 
+// Inisialisasi Farcaster Mini App (SDK, profile, splash)
 await initFarcaster();
-initWalletSlah();
 
-// tiny helper to avoid race: ensure wallet UI wires after DOM ready
-function initSlah() {}
-function initSlah2() {}
-function initSlah3() {}
-function initSlah4() {}
-
-// Just init wallet UI
+// Inisialisasi UI & event wallet (connect, check-in, auth)
 initWalletUI();
